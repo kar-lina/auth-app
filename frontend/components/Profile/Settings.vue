c7584bd05ef2dc070743d8d9894c140ea6945836
kar-lina <akarabutova59@gmail.com>
2024-07-08 13:39:41 +0300

    Fix

--- frontend/components/Profile/Settings.vue
+++ frontend/components/Profile/Settings.vue
@@ -19,7 +19,7 @@
       />
       <ul class="menu bg-base-200 text-base-content min-h-full w-80 p-4">
         <li><a>Sidebar Item 1</a></li>
-        <li><a>Sidebar Item 2</a></li>
+       
       </ul>
     </div>
   </div>
